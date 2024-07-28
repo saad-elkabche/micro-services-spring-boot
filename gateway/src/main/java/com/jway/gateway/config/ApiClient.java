0bc877be5ee7b7c322e665796696366a602840df
elkabche-saad <selkabche@jway.ma>
2024-07-28 23:05:43 +0100

    edit security

--- gateway/src/main/java/com/jway/gateway/config/ApiClient.java
+++ gateway/src/main/java/com/jway/gateway/config/ApiClient.java
@@ -5,14 +5,15 @@ import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.client.RestTemplate;
+import org.springframework.web.reactive.function.client.WebClient;
 
 @Configuration
 public class ApiClient {
 
     @Bean
     @LoadBalanced
-    public RestTemplate restTemplate(){
-        return new RestTemplate();
+    public WebClient.Builder webClientBuilder() {
+        return WebClient.builder();
     }
 
 }
--- gateway/src/main/java/com/jway/gateway/filters/AuthenticationFilter.java
+++ gateway/src/main/java/com/jway/gateway/filters/AuthenticationFilter.java
@@ -6,8 +6,11 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.cloud.gateway.filter.GatewayFilter;
 import org.springframework.cloud.gateway.filter.factory.AbstractGatewayFilterFactory;
 import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Component;
 import org.springframework.web.client.RestTemplate;
+import org.springframework.web.reactive.function.client.WebClient;
+import reactor.core.publisher.Mono;
 
 @Component
 public class AuthenticationFilter extends AbstractGatewayFilterFactory<AuthenticationFilter.Config> {
@@ -15,7 +18,7 @@ public class AuthenticationFilter extends AbstractGatewayFilterFactory<Authentic
     @Autowired
     private RouteValidator validator;
     @Autowired
-    private RestTemplate restTemplate;
+    private WebClient.Builder webClientBuilder;
 
 
     public AuthenticationFilter() {
@@ -29,20 +32,32 @@ public class AuthenticationFilter extends AbstractGatewayFilterFactory<Authentic
             if (validator.isSecured.test(exchange.getRequest())) {
                 //header contains token or not
                 if (!exchange.getRequest().getHeaders().containsKey(HttpHeaders.AUTHORIZATION)) {
-                    throw new RuntimeException("missing authorization header");
+                    exchange.getResponse().setStatusCode(HttpStatus.UNAUTHORIZED);
+                    return exchange.getResponse().setComplete();
                 }
 
                 String authHeader = exchange.getRequest().getHeaders().get(HttpHeaders.AUTHORIZATION).get(0);
 
-                try {
-                    MessageResponse messageResponse = restTemplate.patchForObject("http://AUTHENTICATION/auth/check-token", new RegisterResponse(authHeader), MessageResponse.class);
-                    if(messageResponse.message().equals("invalid")){
-                        throw new RuntimeException("un authorized access to application");
-                    }
-                } catch (Exception e) {
-                    System.out.println("invalid access...!");
-                    throw new RuntimeException(e.getMessage());
-                }
+               return webClientBuilder.build()
+                        .post()
+                        .uri("http://AUTHENTICATION/auth/check-token")
+                        .bodyValue(new RegisterResponse(authHeader))
+                        .retrieve()
+                        .bodyToMono(MessageResponse.class)
+                        .flatMap(messageResponse -> {
+                            System.out.println("message : "+messageResponse.message());
+                           if(!messageResponse.message().equals("valid")){
+                               exchange.getResponse().setStatusCode(HttpStatus.UNAUTHORIZED);
+                               return exchange.getResponse().setComplete();
+                           }
+                           return chain.filter(exchange);
+                        })
+                        .onErrorResume(e -> {
+                            System.out.println(e.getMessage());
+                            exchange.getResponse().setStatusCode(HttpStatus.UNAUTHORIZED);
+                            return exchange.getResponse().setComplete();
+                        });
+
             }
             return chain.filter(exchange);
         });
